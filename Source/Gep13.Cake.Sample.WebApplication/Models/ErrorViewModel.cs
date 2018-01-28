379dc0974e3e74f499d5dc65d312cba9518ca8c5
Gary Ewan Park <gep13@gep13.co.uk>
2018-01-28 20:55:15 +0000

    Added new Sample Project

---
+++ Source/Gep13.Cake.Sample.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gep13.Cake.Sample.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
